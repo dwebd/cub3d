fd0e1ef8d0d90587c8a1bcbf29047c4ec1cf24ac
Diana Mirzoeva <dimirzoe@lab2r2s14.42abudhabi.ae>
2025-05-10 13:01:08 +0400

    get_map_size fixed

--- parsing/parsing_start.c
+++ parsing/parsing_start.c
@@ -41,9 +41,9 @@ bool valid_content(char **file_content, t_main_data *main_data)
         if (i == 6)
             break;
         j++;
+       
     }
-    
-    
+    printf("j before loop: %s", file_content[j]);
     // printf("=== FLOOR COLOR ===\n");
     // printf("Red:   %d\n", main_data->floor.red);
     // printf("Green: %d\n", main_data->floor.green);
@@ -54,7 +54,7 @@ bool valid_content(char **file_content, t_main_data *main_data)
     // printf("Green: %d\n", main_data->ceiling.green);
     // printf("Blue:  %d\n", main_data->ceiling.blue);
 
-        if (!validate_map(file_content, j, main_data))
+        if (!validate_map(file_content, ++j, main_data))
             return (false);
     i = 0;
     while (i < 4)
--- parsing/validate_map.c
+++ parsing/validate_map.c
@@ -1,8 +1,8 @@
 #include "../inc/cub3d.h"
-// bool store_map(char **map_lines, int j, t_main_data main_data)
-// {
+bool store_map(char **map_lines, int j, t_main_data main_data)
+{
     
-// }
+}
 
  static bool get_map_size(char **map_lines, int j, t_main_data *main_data)
 {
@@ -13,8 +13,12 @@
     max_width = 0;
     while(map_lines[j])
     {
-        if(!map_lines[j] || !skip_tab_spaces(map_lines[j]))
+        printf("hello from get_map_size loop, j here = %s\n", map_lines[j]);
+        if(!map_lines[j] || is_empty_line(map_lines[j]))
+        {
+            printf("empty line in the map\n");
             return(false);
+        }
         height++;
         if(ft_strlen(map_lines[j]) > max_width)
             max_width = ft_strlen(map_lines[j]);
@@ -24,6 +28,7 @@
     main_data->map_width = max_width;
     printf("map_height: %zu\n", main_data->map_height);
     printf("map_width: %zu\n", main_data->map_width);
+    printf("j last: %s\n", map_lines[j]);
     
     return (true);
 }
@@ -31,11 +36,16 @@
 bool validate_map(char **map_lines, int j, t_main_data *main_data)
 {
     (void)main_data;
+
+    printf("j после того как попала в функцию validate_map: %s\n", map_lines[j]);
+    while(is_empty_line(map_lines[j]))
+        j++;
+    printf("j после loop пропуска пустых строк: %s", map_lines[j]);
     
     if(!get_map_size(map_lines, j, main_data))
         return(false);
-    // if(!store_map(map_lines, j, main_data))
-    //     return(false);
+    if(!store_map(map_lines, j, main_data))
+        return(false);
     return(true);
    
 }
